f09b667810e240f27ef8018335182e75f1dc291b
abdulkadiret <ethiogentelman@gmail.com>
2020-10-20 21:21:23 +0100

    Add background-image for sidebar with transparent color overlay

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -5,20 +5,22 @@ const Sidebar = (props) => {
   return (
     <aside className='sidebar'>
       <div className='sidebar__left'>
-        <div className='sidebar__header'>
-          <h3 className='sidebar__title'>Shopping Categories</h3>
-          <button className='sidebar__closeButton' onClick={props.closeMenu}>
-            <CloseIcon style={{ fontSize: '2rem' }} />
-          </button>
+        <div className='sidebar__leftOverlay'>
+          <div className='sidebar__header'>
+            <h3 className='sidebar__title'>Shopping Categories</h3>
+            <button className='sidebar__closeButton' onClick={props.closeMenu}>
+              <CloseIcon style={{ fontSize: '2rem' }} />
+            </button>
+          </div>
+          <ul className='categories'>
+            <li className='category'>
+              <a href='index.html'>Shirts</a>
+            </li>
+            <li className='category'>
+              <a href='index.html'>Trousers</a>
+            </li>
+          </ul>
         </div>
-        <ul className='categories'>
-          <li className='category'>
-            <a href='index.html'>Shirts</a>
-          </li>
-          <li className='category'>
-            <a href='index.html'>Trousers</a>
-          </li>
-        </ul>
       </div>
     </aside>
   );
