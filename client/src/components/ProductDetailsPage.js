7d7d5bc3f72f8df939bfcfbd82949090172fe730
abdulkadiret <ethiogentelman@gmail.com>
2020-10-23 22:11:29 +0100

    Add back to main page link

--- client/src/components/ProductDetailsPage.js
+++ client/src/components/ProductDetailsPage.js
@@ -1,53 +1,65 @@
 import React from 'react';
 import data from '../data';
+import { Link } from 'react-router-dom';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 
 const ProductDetailsPage = (props) => {
   const product = data.products.find(
     (item) => item.productId === props.match.params.id
   );
   return (
-    <div className='details'>
-      <div className='details__image'>
-        <img src={product.image} alt='product' />
+    <div>
+      <div className='back__to__homePage'>
+        <Link to='/' className='link__to__homePage'>
+          <ArrowBackIcon
+            style={{ fontSize: '2rem', marginBottom: '-0.4rem' }}
+          />
+          <span>Back to main page</span>
+        </Link>
       </div>
-      <div className='details__info'>
-        <ul>
-          <li>
-            <h3>{product.name}</h3>
-          </li>
-          <li className='product__reviews'>
-            <p>{product.rating} Stars</p>
-            <p>{product.numberOfReviews} Reviews</p>
-          </li>
-          <li>
-            <h3>
-              Price: <span style={{ color: 'red' }}>£{product.price}</span>
-            </h3>
-          </li>
-          <li>
-            <p>Description: {product.description}</p>
-          </li>
-          <li>
-            <p>
-              Brand: <span className='product__brand'>{product.brand}</span>
-            </p>
-          </li>
-        </ul>
-      </div>
-      <div className='details__action'>
-        <ul>
-          <li className='product__price'>Price: {product.price}</li>
-          <li>Status: {product.status}</li>
-          <li className='product__rating'>
-            <label for='qty' size='3' style={{ marginRight: '0.4rem' }}>
-              Qty:
-            </label>
-            <input type='number' step='1' min='0' id='qty' defaultValue='0' />
-          </li>
-          <li>
-            <button className='btn__addToCart'>Add to Cart</button>
-          </li>
-        </ul>
+      <div className='details'>
+        <div className='details__image'>
+          <img src={product.image} alt='product' />
+        </div>
+        <div className='details__info'>
+          <ul>
+            <li>
+              <h3>{product.name}</h3>
+            </li>
+            <li className='product__reviews'>
+              <p>{product.rating} Stars</p>
+              <p>{product.numberOfReviews} Reviews</p>
+            </li>
+            <li>
+              <h3>
+                Price: <span style={{ color: 'red' }}>£{product.price}</span>
+              </h3>
+            </li>
+            <li>
+              <p>Description: {product.description}</p>
+            </li>
+            <li>
+              <p>
+                Brand: <span className='product__brand'>{product.brand}</span>
+              </p>
+            </li>
+          </ul>
+        </div>
+        <div className='details__action'>
+          <ul>
+            <li className='product__price'>Price: {product.price}</li>
+            <li>Status: {product.status}</li>
+            <li className='product__rating'>
+              <label for='qty' size='3' style={{ marginRight: '0.4rem' }}>
+                Qty:
+              </label>
+              <input type='number' step='1' min='0' id='qty' defaultValue='0' />
+            </li>
+            <li>
+              <button className='btn__addToCart'>Add to Cart</button>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
