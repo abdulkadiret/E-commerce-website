035b4a55e245ac903f4a980f94308aab501fd47f
abdulkadiret <ethiogentelman@gmail.com>
2020-10-23 21:54:52 +0100

    Add routes to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
-import Home from './components/Home';
+import Routes from './routes';
 
 function App() {
   return (
-    <div className='app'>
-      <Header />
-      <Home />
-      <Footer />
-    </div>
+    <Router>
+      <div className='app'>
+        <Header />
+        <Routes />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
