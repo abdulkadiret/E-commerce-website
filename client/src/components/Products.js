2ec6fabf054b5bbcf4a49d3aa8957e0fb001e5d1
abdulkadiret <ethiogentelman@gmail.com>
2020-10-25 01:37:16 +0100

    Fetch data from server instead of static data

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import data from '../data';
+import React, { useState, useEffect } from 'react';
 import Product from './Product';
+import axios from 'axios';
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get('/api/products');
+      setProducts(data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <ul className='products'>
-      {data.products.map((product) => (
+      {products.map((product) => (
         <Product
+          key={product.productId}
           productId={product.productId}
           image={product.image}
           name={product.name}
