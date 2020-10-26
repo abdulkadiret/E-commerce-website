d7df23a261a2c0019dca51e1354ad027c463e540
abdulkadiret <ethiogentelman@gmail.com>
2020-10-26 01:22:01 +0000

    Create listProducts action

---
+++ client/src/redux/actions/actions.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import * as actions from './actionTypes';
+
+const listProducts = () => async (dispatch) => {
+  try {
+    dispatch({ type: actions.PRODUCT_LIST_REQUEST });
+    const { data } = await axios.get('/api/products');
+    dispatch({ type: actions.PRODUCT_LIST_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({ type: actions.PRODUCT_LIST_FAIL, payload: error.message });
+  }
+};
+
+export { listProducts };
