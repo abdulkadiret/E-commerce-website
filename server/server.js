1b166a3c84fbfce5b10b88666b1a86c8c1b215c3
abdulkadiret <ethiogentelman@gmail.com>
2020-10-27 14:28:13 +0000

    Create API endpoint to get specific product details  using API call with product id

--- server/server.js
+++ server/server.js
@@ -1,11 +1,20 @@
 import express from 'express';
 const app = express();
-import data from './data';
+import data, { products } from './data';
 
 app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
 
+app.get('/api/products/:id', (req, res) => {
+  const ProductId = req.params.id;
+  const product = data.products.find((x) => x.productId === ProductId);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ msg: 'Product Not Found.' });
+  }
+});
 app.use(express.static('client/build'));
 
 app.listen(5000, () => {
