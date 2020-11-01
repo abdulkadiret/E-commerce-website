9b441c95271295be28ff351d42fc39a209de21fc
abdulkadiret <ethiogentelman@gmail.com>
2020-11-01 22:40:43 +0000

    Create route for cart

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from '../components/Home';
 import ProductDetailsPage from '../components/ProductDetailsPage';
+import CartPage from '../components/CartPage';
 
 export default () => (
   <Switch>
     <Route exact path='/' component={Home} />
     <Route path='/product/:id' component={ProductDetailsPage} />
+    <Route path='/cart/:id?' component={CartPage} />
   </Switch>
 );
