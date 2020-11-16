cabb37a1b9e4f26e735f8aa1ef726347a02f9176
abdulkadiret <ethiogentelman@gmail.com>
2020-11-16 23:51:18 +0000

    Add route for signin component

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 import Home from '../components/Home';
 import ProductDetailsPage from '../components/ProductDetailsPage';
 import CartPage from '../components/CartPage';
+import Signin from '../components/Signin';
 
 export default () => (
   <Switch>
     <Route exact path='/' component={Home} />
     <Route path='/product/:id' component={ProductDetailsPage} />
     <Route path='/cart/:id?' component={CartPage} />
+    <Route path='/signin' component={Signin} />
   </Switch>
 );
